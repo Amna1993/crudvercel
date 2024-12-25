f1459547da5ddb27bfb20bbbe67ea70a70402d44
Amna Razzaq Awan <87529085+Amna1993@users.noreply.github.com>
2024-12-25 10:06:59 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const connectDB = require('./config/db');
 const productRoutes = require('./routes/productRoutes');
 
 const corsOptions = {
-    origin: 'https://crudvercel-zf4b-8c4fegaeb-amna-razzaqs-projects.vercel.app/', // Correctly closed URL
+    origin: 'https://crudvercel-zf4b-83pgvua0f-amna-razzaqs-projects.vercel.app', // Correctly closed URL
     credentials: true,  // Allow credentials
 };
 
